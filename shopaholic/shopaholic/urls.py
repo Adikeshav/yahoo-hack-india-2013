e393d2a69f5fce98811f295ecd9f2fa6c0d98325
theCoder <r@codeyssus.com>
2013-07-14 06:17:31 +0530

    woking on jabong-parsing

--- shopaholic/shopaholic/urls.py
+++ shopaholic/shopaholic/urls.py
@@ -14,6 +14,8 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^parse-jabong/', 'html_parser.views.parse_jabong', name='parse_jabong'),
 )
 
 urlpatterns += patterns('',
